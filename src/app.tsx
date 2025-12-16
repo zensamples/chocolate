bd4612c452ce423ca625fb8533785b98eaa51c51
Eliossul <ltalleceven@gmail.com>
2025-12-16 14:57:57 +0100

    ajout CI

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,25 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import Navbar from "./components/navbar.tsx";
+
+import { LocationProvider, ErrorBoundary, Router, Route } from "preact-iso";
+import NotFound from "./components/not_found.tsx";
+import Home from "./home.tsx";
+import Products from "./components/products.tsx";
+import Social from "./components/social_network.tsx";
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <LocationProvider>
+        <ErrorBoundary>
+          <Navbar />
+          <Router>
+            <Route path="/" component={Home} />
+            <Route path="/products" component={Products} />
+            <Route path="/social_network" component={Social} />
+            <Route component={NotFound} default />
+          </Router>
+        </ErrorBoundary>
+      </LocationProvider>
     </>
-  )
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.tsx'
+import { render } from "preact";
+import { App } from "./app.tsx";
 
-render(<App />, document.getElementById('app')!)
+render(<App />, document.getElementById("app")!);
