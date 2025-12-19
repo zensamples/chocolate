b17d45d8892e48d8f2861cbd7c190903bf46074b
Eliossul <ltalleceven@gmail.com>
2025-12-19 15:48:54 +0100

    ajout du reload des pages

--- src/app.tsx
+++ src/app.tsx
@@ -16,10 +16,12 @@ export function App() {
             <Route path="/chocolatery-showcase" component={Home} />
             <Route path="/" component={Home} />
             <Route path="/chocolatery-showcase/products" component={Products} />
+            <Route path="/products" component={Products} />
             <Route
               path="/chocolatery-showcase/social-network"
               component={Social}
             />
+            <Route path="/social-network" component={Social} />
             <Route component={NotFound} default />
           </Router>
         </ErrorBoundary>
